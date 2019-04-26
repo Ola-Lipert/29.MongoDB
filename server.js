fae4bd3386d4dcf47024c990100fc97f1b157472
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-04-26 23:12:36 +0200

    Add Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://user:<871201!a>@cluster0-49ktl.mongodb.net/test?retryWrites=true', {
     useMongoClient: true
 });
 
